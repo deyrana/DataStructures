31c7e72ea647c130422fb0e2a0c5ec40a0c8ece6
Rana <rana786dey@gmail.com>
2023-10-26 17:36:03 +0530

    bellmanFord algorithm

--- src/graph/ShortestPath.java
+++ src/graph/ShortestPath.java
@@ -43,10 +43,60 @@ public class ShortestPath {
 		g.addEdge(5, 4, 10);
 
 		int[] dist = dijkstra(g, 0);
-		for (int i = 0; i < 9; i++) {
-			System.out.print(dist[i] + " ");
+		System.out.println("dijkstra - ");
+//		for (int i = 0; i < 9; i++) {
+//			System.out.print(dist[i] + " ");
+//		}
+		
+		printArr(dist, g.getV());
+		System.out.println();
+		System.out.println("bellmanFord - ");
+		dist = bellmanFord(g, 0);
+		printArr(dist, g.getV());
+
+	}
+
+	/* ALGORITHM
+	 * 
+	 * 1. Create a dist array to store distance of nodes from source node
+	 * 2. Initialize dist array with 0 for source node and Infinite for other nodes
+	 * 3. Now loop over all the vertices and loop over neighbors of selected vertex.
+	 * 4. For every neighbor, update the distance array if distance of selected node + weight of edge 
+	 * 	  between select node and neighbor is less than distance of neighbor in the dist array.
+	 * 5. Now repeat this for all nodes and all edges V-1 times.
+	 * 6. return the final dist array. 
+	 * 
+	 * */
+	public static int[] bellmanFord(Graph g, int src) {
+		int[][] adjMat = g.getAdjMat();
+		int V = g.getV();
+		int[] dist = new int[V];
+
+		// initialize
+		for (int i = 0; i < V; i++) {
+			dist[i] = Integer.MAX_VALUE;
 		}
 
+		dist[src] = 0;
+
+		for (int i = 0; i < V - 1; i++) {
+			boolean flag = false;
+			for (int u = 0; u < V; u++) {
+				for (int v = 0; v < V; v++) {
+					if (adjMat[u][v] != 0 && dist[u] != Integer.MAX_VALUE) {
+						dist[v] = Integer.min(dist[v], dist[u] + adjMat[u][v]);
+						flag = true;
+					}
+				}
+			}
+			
+			if(!flag) {
+				return dist;
+			}
+			
+		}
+
+		return dist;
 	}
 
 	/* ALGORITHM
@@ -59,42 +109,41 @@ public class ShortestPath {
 	 * 6. From the given nodes select the one node with least distance and not visited (!sptSet[i])
 	 * 7. Mark the selected node as visited in sptSet array
 	 * 8. Now loop for all the neighbors of the selected node
-	 * 9. For every neighbor, update the distance array if distance of selected node + weight of edge 
-	 * 	  between select node and neighbor is less than distance of neighbor in the dist array.
+	 * 9. For every neighbor, update the distance array if neighbor is not visited and 
+	 *    distance of selected node + weight of edge between select node and neighbor is less than 
+	 *    distance of neighbor in the dist array.
 	 * 10. Return the distance array.
 	 * 
 	 * */
 	public static int[] dijkstra(Graph g, int src) {
-		
+
 		int[][] adjMat = g.getAdjMat();
 		int V = g.getV();
-		
+
 		int[] dist = new int[V];
 		boolean[] sptSet = new boolean[V];
-		
-		//initialize
-		for(int i=0; i<V; i++) {
+
+		// initialize
+		for (int i = 0; i < V; i++) {
 			dist[i] = Integer.MAX_VALUE;
 			sptSet[i] = false;
 		}
-		
+
 		dist[src] = 0;
-		
-		for(int vert = 0; vert < V; vert++) {
+
+		for (int vert = 0; vert < V; vert++) {
 			int u = minDist(dist, sptSet, V);
-			
+
 			sptSet[u] = true;
-			
+
 			for (int v = 0; v < V; v++) {
-				if(!sptSet[v] && adjMat[u][v] != 0 
-					&& dist[u] != Integer.MAX_VALUE 
-					&& dist[v] > dist[u] + adjMat[u][v]) {
-					dist[v] = dist[u] + adjMat[u][v];
+				if (!sptSet[v] && adjMat[u][v] != 0 && dist[u] != Integer.MAX_VALUE) {
+					dist[v] = Integer.min(dist[u] + adjMat[u][v], dist[v]);
 				}
 			}
-			
+
 		}
-		
+
 		return dist;
 	}
 
@@ -109,5 +158,12 @@ public class ShortestPath {
 		}
 		return minInd;
 	}
+	
+	public static void printArr(int dist[], int V)
+    {
+        System.out.println("Vertex Distance from Source");
+        for (int i = 0; i < V; ++i)
+            System.out.println(i + "\t\t" + dist[i]);
+    }
 
 }
